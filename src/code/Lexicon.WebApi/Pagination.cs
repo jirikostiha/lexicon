eb80a6b9a912b54e2f64f3526acb3691b41150f9
Jiri Kostiha <ijkdata@gmail.com>
2022-10-18 11:58:11 +0200

    pragma to pagination

--- src/code/Lexicon.WebApi/Pagination.cs
+++ src/code/Lexicon.WebApi/Pagination.cs
@@ -1,10 +1,13 @@
 ﻿namespace Lexicon.WebApi
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     public static class Pagination
+
     {
         public const int PageSizeMin = 4;
         public const int PageSizeMax = 100_000;
         public const int PageNumberMin = 0;
         public const int PageNumberMax = 10_000;
     }
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }

73b5e26fec561b39fed0bfe0eb8c7d678ffe78f8
Jiri Kostiha <ijkdata@gmail.com>
2022-10-20 00:18:31 +0200

    enchant deploy script

--- publishAndDeploy.ps1
+++ publishAndDeploy.ps1
@@ -1,13 +1,19 @@
 dotnet publish .\src\lexicon.sln --output .\asm\app
 
 echo "deploying db(s).."
+try {
+  push-location .\asm\app\
+  
+  .\Lexicon.Cli.exe createDb --sectionName "SQLite-cz"
+  .\Lexicon.Cli.exe import --sectionName "SQLite-cz" --dataFile ".\data-slav_gods.csv"
+  .\Lexicon.Cli.exe import --sectionName "SQLite-cz" --dataFile ".\data-czech_verbs.csv"
 
-.\asm\app\Lexicon.Cli.exe createDb --sectionName "SQLite-cz"
-.\asm\app\Lexicon.Cli.exe import --sectionName "SQLite-cz" --dataFile ".\asm\app\data-slav_gods.csv"
-.\asm\app\Lexicon.Cli.exe import --sectionName "SQLite-cz" --dataFile ".\asm\app\data-czech_verbs.csv"
+  .\Lexicon.Cli.exe createDb --sectionName "SQLite-en"
+  .\Lexicon.Cli.exe import --sectionName "SQLite-en" --dataFile ".\data-english_names.csv"
 
-.\asm\app\Lexicon.Cli.exe createDb --sectionName "SQLite-en"
-.\asm\app\Lexicon.Cli.exe import --sectionName "SQLite-en" --dataFile ".\asm\app\data-english_names.csv"
-
-.\asm\app\Lexicon.Cli.exe createDb --sectionName "SQLite-gr"
-.\asm\app\Lexicon.Cli.exe import --sectionName "SQLite-gr" --dataFile ".\asm\app\data-greek_gods.csv"
+  .\Lexicon.Cli.exe createDb --sectionName "SQLite-gr"
+  .\Lexicon.Cli.exe import --sectionName "SQLite-gr" --dataFile ".\data-greek_gods.csv"
+}
+finally {
+  pop-location
+}

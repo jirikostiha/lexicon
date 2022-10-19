9e810c54ead04e097d66f8e69575c9a62d1c4b86
Jiri Kostiha <ijkdata@gmail.com>
2022-10-19 10:25:51 +0200

    provider

---
+++ src/code/Lexicon.Data/CachingProvider.cs
@@ -0,0 +1,29 @@
+﻿namespace Lexicon.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text.Json;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Lexicon.EntityModel;
+
+    public class CachingProvider : IWordProvider
+    {
+        private readonly IWordProvider _underlyingProvider;
+
+        public CachingProvider(IWordProvider underlyingProvider)
+        {
+            _underlyingProvider = underlyingProvider;
+        }
+
+        public Task<long> CountAsync(CancellationToken ct = default)
+            =>  _underlyingProvider.CountAsync(ct);
+
+        public Task<IEnumerable<WordRecord>> GetByFilterAsync(WordFilter filter, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

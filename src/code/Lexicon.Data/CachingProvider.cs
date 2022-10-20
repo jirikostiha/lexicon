5d72460d131ea5c0a84453696b50c63008a82d3e
Jiri Kostiha <ijkdata@gmail.com>
2022-10-20 13:20:00 +0200

    memory cache

--- src/code/Lexicon.Data/CachingProvider.cs
+++ src/code/Lexicon.Data/CachingProvider.cs
@@ -2,28 +2,36 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.IO;
-    using System.Text.Json;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
     using Lexicon.EntityModel;
+    using Microsoft.Extensions.Caching.Memory;
 
     public class CachingProvider : IWordProvider
     {
         private readonly IWordProvider _underlyingProvider;
+        private readonly IMemoryCache _cache;
 
-        public CachingProvider(IWordProvider underlyingProvider)
+        public CachingProvider(IWordProvider underlyingProvider, IMemoryCache cache)
         {
             _underlyingProvider = underlyingProvider;
+            _cache = cache;
         }
 
-        public Task<long> CountAsync(CancellationToken ct = default)
-            =>  _underlyingProvider.CountAsync(ct);
-
-        public Task<IEnumerable<WordRecord>> GetByFilterAsync(WordFilter filter, CancellationToken ct = default)
+        public async Task<long> CountAsync(CancellationToken ct = default)
         {
-            throw new NotImplementedException();
+            return await _cache.GetOrCreateAsync(
+                nameof(CountAsync),
+                async _ => await _underlyingProvider.CountAsync(ct)
+                    .ConfigureAwait(false));
+        }
+
+        public async Task<IEnumerable<WordRecord>> GetByFilterAsync(WordFilter filter, CancellationToken ct = default)
+        {
+            return await _cache.GetOrCreateAsync(
+                filter.ToString(), 
+                async _ => await _underlyingProvider.GetByFilterAsync(filter, ct)
+                    .ConfigureAwait(false));
         }
     }
 }

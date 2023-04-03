f74cd6ae040eeef3bb1ddfcd71365aefabaf23bd
Jiri Kostiha <ijkdata@gmail.com>
2023-04-03 17:56:43 +0200

    ci(commitlint_config): extend max line length

--- .github/commitlint_config.js
+++ .github/commitlint_config.js
@@ -1,10 +1,10 @@
 module.exports = {
     rules: {
         "body-leading-blank": [1, "always"],
-        "body-max-line-length": [2, "always", 100],
+        "body-max-line-length": [2, "always", 160],
         "footer-leading-blank": [1, "always"],
-        "footer-max-line-length": [2, "always", 100],
-        "header-max-length": [2, "always", 72],
+        "footer-max-line-length": [2, "always", 160],
+        "header-max-length": [2, "always", 100],
         "scope-case": [2, "always", "lower-case"],
         "subject-case": [
             2,

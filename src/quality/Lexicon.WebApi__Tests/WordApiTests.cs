e131d3ecd047d0ea96c08b02b106cdc9f20c9d1e
Jiri Kostiha <ijkdata@gmail.com>
2023-04-27 18:53:08 +0200

    test(api): word api test
    using fluentassertions and WebApplicationFactory

---
+++ src/quality/Lexicon.WebApi__Tests/WordApiTests.cs
@@ -0,0 +1,42 @@
+﻿namespace Lexicon.WebApi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using FakeItEasy;
+    using FluentAssertions;
+    using Lexicon.EntityModel;
+    using Lexicon.TestReady;
+    using Lexicon.WebApi.Controllers;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging.Abstractions;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Mvc.Testing;
+    using Xunit;
+    using System.Security.Policy;
+
+    public class WordApiTests
+    {
+        private readonly WebApplicationFactory<Program> _factory;
+
+        public WordApiTests()
+        {
+            _factory = new WebApplicationFactory<Program>();
+        }
+
+        [Theory]
+        [Trait("Category", "positive")]
+        [InlineData("/api/words")]
+        [InlineData("/api/words?page=0&pageSize=100")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            using var client = _factory.CreateClient();
+
+            var response = await client.GetAsync(url);
+
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            response.Content.Headers.ContentType.ToString().Should().Be("text/html; charset=utf-8");
+        }
+    }
+}
--- src/quality/Lexicon.WebApi__Tests/WordControllerTests.cs
+++ src/quality/Lexicon.WebApi__Tests/WordControllerTests.cs
@@ -1,17 +1,19 @@
 ﻿namespace Lexicon.WebApi
 {
     using FakeItEasy;
+    using FluentAssertions;
     using Lexicon.EntityModel;
     using Lexicon.TestReady;
     using Lexicon.WebApi.Controllers;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Logging;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Mvc.Testing;
     using Microsoft.Extensions.Logging.Abstractions;
     using System.Collections.Generic;
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Xunit;
     using System.Threading.Tasks;
 
-    [TestClass]
     public class WordControllerTests
     {
         private readonly ILogger<WordsController> _logger = new NullLogger<WordsController>();
@@ -20,16 +22,15 @@
         private WordMultiSourceProvider _multiSourceProvider;
         private WordsController _controller;
 
-        [TestInitialize]
-        public void SetUp()
+        public WordControllerTests()
         {
             var providers = new List<(string, IWordProvider)>() { ("fake", _provider) };
             _multiSourceProvider = new WordMultiSourceProvider(new SourceProvider(providers));
             _controller = new WordsController(_multiSourceProvider, _logger);
         }
 
-        [TestMethod]
-        [TestCategory("positive")]
+        [Fact()]
+        [Trait("Category", "positive")]
         public async Task Get_EmptyFilter_FirstPage()
         {
             var filter = MultiSourceWordFilter.Empty;
@@ -38,9 +39,10 @@
 
             var result = await _controller.Get(0, 10, filter);
 
-            Assert.IsNotNull(result);
+            result.Should().NotBeNull();
+
             var dataPage = (DataPage<WordRecord>)((OkObjectResult)result.Result)?.Value;
-            Assert.AreEqual(10, dataPage.Items.Count);
+            dataPage.Items.Count.Should().Be(10);
         }
     }
 }

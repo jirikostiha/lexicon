4220ba42fe5cf455ea5bdeb703cf956fe355e3bf
Jiri Kostiha <ijkdata@gmail.com>
2023-08-28 09:30:09 +0200

    test: comment to avoid warning

--- src/quality/Lexicon.EntityFramework__Tests/WordRecordContextTests.cs
+++ src/quality/Lexicon.EntityFramework__Tests/WordRecordContextTests.cs
@@ -3,14 +3,14 @@ using Xunit;
 
 namespace Lexicon.EntityFramework;
 
-public class WordRecordContextTests
-{
-    [Fact]
-    public async Task CountAsync_AllRecords_Match()
-    {
-        //todo
-        //var count = await repo.CountAsync();
+//public class WordRecordContextTests
+//{
+//    [Fact]
+//    public async Task CountAsync_AllRecords_Match()
+//    {
+//        //todo
+//        //var count = await repo.CountAsync();
 
-        //Assert.Equal(WordSets.All.Count(), count);
-    }
-}
+//        //Assert.Equal(WordSets.All.Count(), count);
+//    }
+//}
